ea9cc7c8d71e36579ba9e84a79c0cf618e32343d
Luke Veitch <luke.veitch@live.co.uk>
2022-05-06 11:46:05 +0100

    ascii text

---
+++ scrape.py
@@ -0,0 +1,27 @@
+from bs4 import BeautifulSoup
+import requests as r
+import pandas as pd
+
+
+response = r.get('https://www.teenvogue.com/gallery/best-harry-potter-quotes')
+content = response.text
+soup = BeautifulSoup(content, 'html.parser')
+
+olTags = soup.find_all('ol')
+all_quotes = []
+for idx, tags in enumerate(olTags):
+    for quote in olTags[idx]:
+        line = quote.text
+        line = line.encode('ascii', 'ignore').decode('ascii')
+        all_quotes.append(line)
+
+df = pd.DataFrame({'Quotes' : all_quotes})
+df.to_csv('HP_quotes.csv', index = False)
+
+
+
+
+    
+
+
+
