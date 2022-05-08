827c330ed48d538e1cd43f06046b56f0220eb650
Luke Veitch <63455910+lukeveitch@users.noreply.github.com>
2022-05-08 13:44:25 +0100

    second script

--- scrape2.py
+++ scrape2.py
@@ -5,12 +5,14 @@ import pandas as pd
 response = requests.get('https://www.hypable.com/best-harry-potter-quotes-books-movies/')
 soup = BeautifulSoup(response.text, 'html.parser')
 
-divTag = soup.find_all("ol")
+olTags = soup.find_all('ol')
+all_quotes = []
 
-lst = []
-for line in divTag:
-    row = line.text
-    lst.append(row)
+for idx, tags in enumerate(olTags):
+    for quote in olTags[idx]:
+        line = quote.text
+        line = line.encode('ascii', 'ignore').decode('ascii')
+        all_quotes.append(line)
 
-df = pd.DataFrame({"quote": lst})
-df.to_csv('HarryPotterQuotes2.csv')
+df = pd.DataFrame({'Quotes' : all_quotes})
+df.to_csv('HarryPotter_quotes2.csv', index = False)
