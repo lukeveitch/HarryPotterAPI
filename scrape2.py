8912aa8e7216f9146e68a7630fdbcce53ced243c
Luke Veitch <63455910+lukeveitch@users.noreply.github.com>
2022-05-08 13:36:37 +0100

    finished (not tested yet)

--- scrape2.py
+++ scrape2.py
@@ -1,3 +1,16 @@
 import requests
 from bs4 import BeautifulSoup
+import pandas as pd
 
+response = requests.get('https://www.hypable.com/best-harry-potter-quotes-books-movies/')
+soup = BeautifulSoup(response.text, 'html.parser')
+
+divTag = soup.find_all("ol")
+
+lst = []
+for line in divTag:
+    row = line.text
+    lst.append(row)
+
+df = pd.DataFrame({"quote": lst})
+df.to_csv('HarryPotterQuotes2.csv')
